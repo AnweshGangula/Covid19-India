5f392ea8cd455735f22bdab5843812a298278ad0
Gangula2 <gangulaanwesh@gmail.com>
2021-03-02 14:11:11 +0530

    Line Chart Tooltip delay
     - Added delay in tooltip - to show once the line-chart transition is completed (https://stackoverflow.com/a/56974706/6908282)
     - changed tooltip rectangle size
     - made bisect and callout functions movable (if necessary)

--- js/charts/Active Cases line.js
+++ js/charts/Active Cases line.js
@@ -1,5 +1,6 @@
 import gVar, { formatDate, formatValue } from "../global var.js";
 
+const duration = 2000;
 class Active_Case_Line {
   constructor(element, data) {
     this.element = element;
@@ -20,6 +21,7 @@ class Active_Case_Line {
       .classed("svg-container", true);
 
     this.plot = chart.append("g").attr("fill", "royalblue");
+    this.chart = chart;
 
     this.createScales();
     this.drawAxis();
@@ -110,84 +112,102 @@ class Active_Case_Line {
     path
       .transition()
       .ease(d3.easeBackOut)
-      .duration(2000)
+      .duration(duration)
       .attr("d", valueline_animate);
 
     area
       .transition()
       .ease(d3.easeBackOut)
-      .duration(2000)
+      .duration(duration)
       .attr("d", area_path_animate);
 
     // Tooltip Source: https://observablehq.com/@d3/line-chart-with-tooltip
-    const tooltip = this.plot.append("g");
+    const tooltip = this.plot.append("g").style("display", "none");
 
-    const x_val = (x) => this.xScale(x);
-    const y_val = (y) => this.yScale(y);
-    visual.on("touchmove mousemove", function(event) {
-      const {date, value} = bisectFun(d3.pointer(event, this)[0]);
-      // console.log({date, value})
-      // console.log(this.yScale(500))
+    const sleep = (milliseconds) => {
+      return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    };
+
+    //wait till line Animation is completed - source: https://stackoverflow.com/a/56974706/6908282
+    sleep(duration).then(() => {
       tooltip
+        .append("circle")
+        .attr("r", 4)
+        .attr("fill", "white")
+        .attr("stroke", "royalblue")
+        .attr("stroke-width", 2);
+
+      const Xscale = this.xScale;
+      const x_val = (x) => this.xScale(x);
+      const y_val = (y) => this.yScale(y);
+      const all_data = this.data;;
+      this.chart.on("touchmove mousemove", function (event) {
+        const { date, value } = bisectFun(d3.pointer(event, this)[0], Xscale, all_data);
+        // console.log({date, value})
+        // console.log(this.yScale(500))
+        tooltip
           .attr("transform", `translate(${x_val(date)},${y_val(value)})`)
           // .attr("transform", 'translate(50,0)')
           .call(callout, `${formatDate(date)}~${formatValue(parseInt(value))}`);
-    });
-  
-    visual.on("touchend mouseleave", () => tooltip.call(callout, null));
+      });
 
-    const bisectFun = (mx) => {
-      const bisect = d3.bisector((d) => d.Date_YMD).left;
-        const date = this.xScale.invert(mx);
-        const index = bisect(this.data, date, 1);
-        const a = this.data[index - 1];
-        const b = this.data[index];
-        return b && date - a.Date_YMD > b.Date_YMD - date ? {date: b.Date_YMD, value: b.Active} : {date: a.Date_YMD, value: a.Active};
-    }
+      this.chart.on("touchend mouseleave", () => tooltip.call(callout, null));
 
-    const callout = (g, value) => {
-      if (!value) return g.style("display", "none");
-  
-      g.style("display", null)
-        .style("pointer-events", "none")
-        .style("font", "10px sans-serif");
-  
-      const path = g
-        .selectAll("path")
-        .data([null])
-        .join("path")
-        .attr("fill", "white")
-        .attr("stroke", "black");
-  
-      const text = g
-        .selectAll("text")
-        .data([null])
-        .join("text")
+      // Below functions ( bisectFun, callout) can be moved to separate module(js file) if necessary
 
-      const { x, y, width: w, height: h } = text.node().getBBox();
-      
-      text
-        .call((text) =>
+      const bisectFun = (mx, XScale, data) => {
+        const bisect = d3.bisector((d) => d.Date_YMD).left;
+        const date = XScale.invert(mx);
+        const index = bisect(data, date, 1);
+        const a = data[index - 1];
+        const b = data[index];
+        return b && date - a.Date_YMD > b.Date_YMD - date
+          ? { date: b.Date_YMD, value: b.Active }
+          : { date: a.Date_YMD, value: a.Active };
+      };
+
+      const callout = (g, value) => {
+        if (!value) return g.style("display", "none");
+
+        g.style("display", null)
+          .style("pointer-events", "none")
+          .style("font", "10px sans-serif");
+
+        const path = g
+          .selectAll("path")
+          .data([null])
+          .join("path")
+          .attr("fill", "white")
+          .attr("stroke", "black");
+
+        const text = g.selectAll("text").data([null]).join("text");
+
+        const { x, y, width: w, height: h } = text.node().getBBox();
+
+        text.call((text) =>
           text
             .selectAll("tspan")
             .data((value + "").split(/~/))
             .join("tspan")
-            .attr("x", w/2)
+            .attr("x", w / 2)
             .attr("y", (d, i) => `${i * 1.1}em`)
+            .attr("width", w)
+            .attr("height", h)
             .attr("text-anchor", "middle")
             .style("font-weight", (_, i) => (i ? null : "bold"))
             .text((d) => d.trim())
         );
-  
-  
-      text.attr("transform", `translate(${-w / 2},${15 - y})`);
-      path.attr(
-        "d",
-        `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`
-      );
-    };
-  }
 
+        text.attr("transform", `translate(${-w / 2},${13 - y})`);
+        path.attr(
+            "d",
+            `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 10}h-${w + 20}z`
+          )
+          .attr("transform", `translate(0,3)`);
+      };
+    });;
+  }
+  
 }
 
 export default Active_Case_Line;
